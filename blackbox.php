5f45c9386708057b7e111d1ddd44bf6899bc5376
Baptiste Langlade <baptouuuu@gmail.com>
2024-03-24 15:05:00 +0100

    only run one scenario when applying code coverage

--- blackbox.php
+++ blackbox.php
@@ -12,14 +12,16 @@ use Innmind\BlackBox\{
 Application::new($argv)
     ->when(
         \getenv('ENABLE_COVERAGE') !== false,
-        static fn(Application $app) => $app->codeCoverage(
-            CodeCoverage::of(
-                __DIR__.'/src/',
-                __DIR__.'/proofs/',
+        static fn(Application $app) => $app
+            ->codeCoverage(
+                CodeCoverage::of(
+                    __DIR__.'/src/',
+                    __DIR__.'/proofs/',
+                )
+                    ->dumpTo('coverage.clover')
+                    ->enableWhen(true),
             )
-                ->dumpTo('coverage.clover')
-                ->enableWhen(true),
-        ),
+            ->scenariiPerProof(1),
     )
     ->tryToProve(Load::directory(__DIR__.'/tests'))
     ->exit();
